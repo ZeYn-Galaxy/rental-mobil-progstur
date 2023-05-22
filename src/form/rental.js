361a0f620db95d1683a9eadcf01a610b4a8ea912
ZeYn-Galaxy <shafwan066@gmail.com>
2023-05-22 23:22:37 +0800

    p

--- src/form/rental.js
+++ src/form/rental.js
@@ -1,4 +1,4 @@
-import data from "../../data.json" assert { type: 'json' };
+import data from "../data.json" assert { type: 'json' };
 
 let rental = sessionStorage.getItem('rental')
 document.title = rental
